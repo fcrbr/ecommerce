3262ba01ef712541fdef4d48968b3d9bab4218ba
fcrbr <filipecr@gmail.com>
2023-01-14 11:41:29 -0300

    criado o PageAdmin e o tpl admin

---
+++ vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace Hcode;
+use Rain\Tpl;
+ 
+class PageAdmin extends Page {
+    
+    public function __construct($opts = array(), $tpl_dir = "/views/soquemadm/") 
+    {
+        parent::__construct($opts, $tpl_dir);
+    }
+}
+
+?>
+
