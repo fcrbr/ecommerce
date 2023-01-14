0b38bfe3028b4c1cb4eb6cafc700500eb014197a
fcrbr <filipecr@gmail.com>
2023-01-14 16:21:07 -0300

    tela de login

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php 
+session_start();
 
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
@@ -20,10 +22,34 @@ $app->get('/', function() {
 
 
 $app->get('/soquemadm/', function() { 
+    User::verifyLogin();
     $page = new PageAdmin();
  
     $page->setTpl("index"); 
  
+});
+
+$app->get('/soquemadm/login/', function() { 
+    $page = new PageAdmin([
+        "header"=>false,
+        "footer"=>false
+    ]);
+ 
+    $page->setTpl("login"); 
+ 
+});
+
+$app->post('/soquemadm/login', function() {
+
+    User::login($_POST["login"], $_POST["password"]);
+    header("Location: /soquemadm");
+    exit;
+});
+
+$app->get('/soquemadm/logout', function() {
+    User::logout();
+    header("Location: /soquemadm/login");
+    exit;
 });
 
 
